d94b9cd6508852f7763b2de135182b0bbecacfea
yuksel daskin <yuksel_daskin@outlook.com>
2023-09-29 18:14:06 +0200

    yearly salary rule test

---
+++ ApiTests/UnitTests/YearlySalaryExceedRuleTest.cs
@@ -0,0 +1,54 @@
+﻿using Api.Calculation.CalculationRules;
+using System;
+using Xunit;
+
+namespace ApiTests.UnitTests
+{
+    public class YearlySalaryExceedRuleTest : CalculationRulesTest
+    {
+
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(0, false)]
+        public void CheckEmployeeLowSalary(decimal expected, bool forTwicePaid)
+        {
+            var rule = new YearlySalaryExceedRule
+            {
+                WeeksForYear = WpCalculation.Item1,
+                PaycheckCountForMonth = WpCalculation.Item2
+            };
+            var reflist = forTwicePaid ? WeekNumbersForTwicePaidMonth : WeekNumbersForThricePaidMonth;
+            foreach (var weekNo in reflist)
+            {
+                if (!rule.Eligible(EmployeeWithoutDependent, weekNo)) continue;
+                var effect = rule.Effect(EmployeeWithoutDependent, weekNo);
+                Assert.Equal(expected, Decimal.Round(effect, 2));
+
+            }
+        }
+        [Theory]
+        [InlineData(-200, true)]
+        [InlineData(-200, false)]
+        public void CheckEmployeeHiSalary(decimal expected, bool forTwicePaid)
+        {
+            var rule = new YearlySalaryExceedRule
+            {
+                WeeksForYear = WpCalculation.Item1,
+                PaycheckCountForMonth = WpCalculation.Item2
+            };
+            var salary = EmployeeWithoutDependent.Salary;
+            EmployeeWithoutDependent.Salary = 260000m;
+            var reflist = forTwicePaid ? WeekNumbersForTwicePaidMonth : WeekNumbersForThricePaidMonth;
+            foreach (var weekNo in reflist)
+            {
+                if (!rule.Eligible(EmployeeWithoutDependent, weekNo)) continue;
+                var effect = rule.Effect(EmployeeWithoutDependent, weekNo);
+                Assert.Equal(expected, Decimal.Round(effect, 2));
+
+            }
+            EmployeeWithoutDependent.Salary = salary;
+
+        }
+
+    }
+}
